04bdd913bf746577f2981f8134a5f953234215d9
Azat <8280770+azataiot@users.noreply.github.com>
2023-01-07 15:56:22 +0300

    upd

--- fe507/__init__.py
+++ fe507/__init__.py
@@ -2,7 +2,7 @@
 # Created by azat at 1.01.2023
 
 """FE507 is a simple yet very powerful, 'batteries included' intuitive package for data analysing."""
-__version__ = "0.4.5-alpha"
+__version__ = "0.4.6-alpha"
 
 from fe507.settings import settings
 from fe507.fe507 import *
