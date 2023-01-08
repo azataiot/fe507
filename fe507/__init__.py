717472f269fa7c3464c7deade74a944bbfd1c2ae
Azat <8280770+azataiot@users.noreply.github.com>
2023-01-08 11:19:58 +0300

    refactoring code

--- fe507/__init__.py
+++ fe507/__init__.py
@@ -2,7 +2,7 @@
 # Created by azat at 1.01.2023
 
 """FE507 is a simple yet very powerful, 'batteries included' intuitive package for data analysing."""
-__version__ = "0.4.8-alpha"
+__version__ = "0.4.9-alpha"
 
 from fe507.settings import settings
 from fe507.fe507 import *
