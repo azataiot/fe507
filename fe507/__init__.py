1cd2ef2e2f6d2c212d90d063e9e66ec5e942960e
Azat <8280770+azataiot@users.noreply.github.com>
2023-01-08 10:19:30 +0300

    pub

--- fe507/__init__.py
+++ fe507/__init__.py
@@ -2,7 +2,7 @@
 # Created by azat at 1.01.2023
 
 """FE507 is a simple yet very powerful, 'batteries included' intuitive package for data analysing."""
-__version__ = "0.4.6-alpha"
+__version__ = "0.4.7-alpha"
 
 from fe507.settings import settings
 from fe507.fe507 import *
@@ -11,4 +11,3 @@ from fe507.collection import *
 '''
 https://flit.pypa.io/en/latest/index.html
 '''
-
